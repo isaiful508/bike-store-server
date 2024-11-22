3b51a4432b727ebe0ab31a602b9099463c4d174c
Md. Saiful islam <isaiful.islam508@gmail.com>
2024-11-22 23:17:19 +0600

    eslint and prettier set up

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,14 @@
-const express = require('express')
-const app = express()
-const port = 5000
+import express, { Application, Request, Response } from 'express'
+import cors from 'cors'
+const app : Application = express()
 
-app.get('/', (req, res) => {
+//parser
+app.use(express.json())
+app.use(cors())
+
+
+app.get('/', (req : Request, res: Response) => {
     res.send('Hello World!')
 })
 
+export default app;
